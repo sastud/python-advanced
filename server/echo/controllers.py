9b54b5383d4d32a1e079c1369c4d77b76847c295
sastud <svinarev.sa@yandex.ru>
2019-06-18 03:31:00 +0300

    ДЗ-6: добавлены декораторы логирования, проверки логина пользователя по хешу в поле user запроса дбавлен middleware работы со сжатыми данными обработчик пользовательских запросов вынесен в отдельный модуль handlers добавлен fabric3

--- server/echo/controllers.py
+++ server/echo/controllers.py
@@ -1,6 +1,13 @@
 from protocol import make_response
+# импортируем декораторы logger_required и login_required
+from decorators import (
+    logger_required,
+    login_required
+)
 
 # функция эхо-сервера
+@login_required     # декорируем функцию эхо-сервера докоратором проверки логина
+@logger_required    # декорируем функцию эхо-сервера докоратором логирования
 def get_echo(request):
     data = request.get('data')
     return make_response(
